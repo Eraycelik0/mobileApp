8ab9feb7b4e99e698721abd1a5a4fdded21e1b11
Eraycelik0 <celikeray39@gmail.com>
2024-03-23 16:05:58 +0300

    commit

---
+++ backend/app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv/config");
+const authJwt = require('./helpers/jwt');
+
+app.use(cors());
+app.options("*", cors());
+
+// Middleware
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(authJwt);
+
+// Routers
+const categoriesRoutes = require("./routes/categories");
+const productsRoutes = require("./routes/products");
+const usersRoutes = require("./routes/users");
+const ordersRoutes = require("./routes/orders");
+
+const api = process.env.API_URL;
+
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
+
+//Database
+mongoose
+  .connect(process.env.CONNECTION_STRING)
+  .then(() => {
+    console.log("Database Connection is ready...");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(3000, () => {
+  console.log(api);
+  console.log("server is running http://localhost:3000");
+});
