919bf061017da10122d150c908f93ce358b27529
Eraycelik0 <celikeray39@gmail.com>
2024-03-31 15:13:32 +0300

    users authentication and orders-product-image added.

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,6 +4,7 @@ const {Category} = require("../models/category");
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const {Product} = require("../models/product");
 
 router.get(`/`, async (req, res) => {
     const userList = await User.find().select('-passwordHash');
@@ -49,31 +50,64 @@ router.post("/", async (req, res) => {
 });
 
 router.post('/login', async (req, res) => {
-  const user = await User.findOne(
-      {
-        email: req.body.email
-      }
-  );
-  const secret = process.env.secret;
+    const user = await User.findOne(
+        {
+            email: req.body.email
+        }
+    );
+    const secret = process.env.secret;
 
-  if (!user) {
-    return res.status(400).send('The user not found!');
-  }
+    if (!user) {
+        return res.status(400).send('The user not found!');
+    }
 
-  if (user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
-      const token = jwt.sign({
-          userId: user.id,
-      },
-        secret,
-          {expiresIn: '1d'}
-      )
+    if (user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
+        const token = jwt.sign({
+                userId: user.id,
+                isAdmin: user.isAdmin
+            },
+            secret,
+            {expiresIn: '1d'}
+        )
 
-    return res.status(200).send({user: user.email, token: token});
-  } else {
-    return res.status(400).send('password is wrong!');
-  }
+        return res.status(200).send({user: user.email, token: token});
+    } else {
+        return res.status(400).send('password is wrong!');
+    }
 
 
+});
+router.get(`/get/count`, async (req, res) => {
+    try {
+        const userCount = await User.countDocuments();
+        res.send({
+            userCount: userCount
+        });
+    } catch (error) {
+        res.status(500).json({ success: false, error: error.message });
+    }
+});
+
+router.delete("/:id", (req, res) => {
+    User.findOneAndDelete({ _id: req.params.id })
+        .then((user) => {
+            if (user) {
+                return res.status(200).json({
+                    success: true,
+                    message: "The user is deleted!",
+                });
+            } else {
+                return res
+                    .status(404)
+                    .json({ success: false, message: "user not found!" });
+            }
+        })
+        .catch((err) => {
+            return res.status(400).json({
+                success: false,
+                error: err.message,
+            });
+        });
 });
 
 module.exports = router;
